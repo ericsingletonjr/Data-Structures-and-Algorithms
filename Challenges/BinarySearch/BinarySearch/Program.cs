6dd944fc1365b2ef2180065c3a626d4b4e5b212b
Eric Singleton <eric.singleton.jr@gmail.com>
2018-06-05 21:47:41 -0700

    Adjusted algorithm to correctly be a Binary Search

--- Challenges/BinarySearch/BinarySearch/Program.cs
+++ Challenges/BinarySearch/BinarySearch/Program.cs
@@ -6,17 +6,27 @@ namespace BinarySearch
     {
         static void Main(string[] args)
         {
-            int[] dataSet = new int[] { 4, 8, 15, 16, 23, 42 };
+            int[] dataSet = new int[] { 4, 8, 15, 16, 23, 42, 50, 65, 72 };
             int searchKey = 16;
 
-            Console.WriteLine(BinarySearch(dataSet, searchKey));
-            Console.ReadLine();
+           BinarySearch(dataSet, searchKey);
         }
         public static int BinarySearch(int[] dataSet, int searchKey)
         {
-            for(int i = 0; i < dataSet.Length; i++)
+            //Sets minimum and maximum of each half
+            int minThreshold = 0;
+            int maxThreshold = dataSet.Length - 1;
+
+            while(maxThreshold >= minThreshold)
             {
-                if (dataSet[i] == searchKey) return i;
+                //Find the middle of my dataSet
+                int middle = (minThreshold + maxThreshold) / 2;
+
+                //Logic to determine if the new half needs to be shifted up
+                //Or down in relation to the search key
+                if (dataSet[middle] > searchKey) maxThreshold = middle - 1;
+                if (dataSet[middle] < searchKey) minThreshold = middle + 1;
+                if (dataSet[middle] == searchKey) return middle;
             }
             return -1;
         }
