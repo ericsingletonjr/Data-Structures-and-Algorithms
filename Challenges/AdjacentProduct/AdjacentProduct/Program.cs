11619dfb703a46c5fbb5bb47e83e8e097be63598
Eric Singleton <eric.singleton.jr@gmail.com>
2018-06-06 19:26:54 -0700

    Added tests and solution

---
+++ Challenges/AdjacentProduct/AdjacentProduct/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace AdjacentProduct
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] dataSet = new int[,] { { 1, 2, 1 }, { 2, 4, 2 }, { 3, 6, 8 }, { 7, 8, 1 } };
+           
+
+            Console.WriteLine(AdjacentProduct(dataSet));
+        }
+        public static int AdjacentProduct(int[,] dataSet)
+        {
+            int largestValue = 0, currentValue,  dataSetJ = dataSet.GetLength(0), dataSetI = dataSet.GetLength(1);
+
+            for(int j = 0; j < dataSet.GetLength(0); j++)
+            {
+                for(int i = 0; i < dataSet.GetLength(1); i++)
+                {
+
+                    if ((i + 1) < dataSetI && (j + 1) < dataSetJ)
+                    {
+                        currentValue = dataSet[j, i] * dataSet[j, i + 1];
+                        largestValue = currentValue > largestValue ? currentValue : largestValue;
+                        currentValue = dataSet[j, i] * dataSet[j + 1, i];
+                        largestValue = currentValue > largestValue ? currentValue : largestValue;
+                        currentValue = dataSet[j, i] * dataSet[j + 1, i+1];
+                        largestValue = currentValue > largestValue ? currentValue : largestValue;
+                    }
+                    if (j == dataSetJ - 1 && i+1 < dataSetI)
+                    {
+                        currentValue = dataSet[j, i] * dataSet[j, i + 1];
+                        largestValue = currentValue > largestValue ? currentValue : largestValue;
+                        currentValue = dataSet[j, i] * dataSet[j - 1, i + 1];
+                        largestValue = currentValue > largestValue ? currentValue : largestValue;
+                    }
+                }
+            }           
+            return largestValue;
+        }
+    }
+}
---
+++ Challenges/AdjacentProduct/AdjacentProductTests/UnitTest1.cs
@@ -0,0 +1,35 @@
+using System;
+using AdjacentProduct;
+using Xunit;
+
+namespace AdjacentProductTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void largestValue1()
+        {
+            int[,] dataSet = new int[,] { { 1, 2, 1 }, { 2, 4, 2 }, { 3, 6, 8 }, { 7, 8, 1 } };
+
+            Assert.Equal(64, Program.AdjacentProduct(dataSet));
+        }
+
+        [Fact]
+        public void largestValue2()
+        {
+            int[,] dataSet = new int[,] { { 6, 3, 5, 9 }, { 2, 8, 4, 6 }, { 3, 8, 2, 1 }, { 6, 7, 3, 5 } };
+
+            Assert.Equal(64, Program.AdjacentProduct(dataSet));
+        }
+
+        [Fact]
+        public void largestValue3()
+        {
+            int[,] dataSet = new int[,] { {2,5,8,6,7 }, { 10,3,4,9,8}, 
+                { 1,6,15,11,2}, { 8,4,6,2,8} ,{ 9,20,2,4,10} };
+
+            Assert.Equal(180, Program.AdjacentProduct(dataSet));
+        }
+
+    }
+}
