fd977795e870f547d30ee7cabc9c38518fcd228d
Eric Singleton <eric.singleton.jr@gmail.com>
2018-06-07 09:50:38 -0700

    Refactored code to follow Single Responsibility and put complex logic in smaller methods

--- Challenges/AdjacentProduct/AdjacentProduct/Program.cs
+++ Challenges/AdjacentProduct/AdjacentProduct/Program.cs
@@ -7,13 +7,13 @@ namespace AdjacentProduct
         static void Main(string[] args)
         {
             int[,] dataSet = new int[,] { { 1, 2, 1 }, { 2, 4, 2 }, { 3, 6, 8 }, { 7, 8, 1 } };
-           
 
             Console.WriteLine(AdjacentProduct(dataSet));
         }
+
         public static int AdjacentProduct(int[,] dataSet)
         {
-            int largestValue = 0, currentValue,  dataSetJ = dataSet.GetLength(0), dataSetI = dataSet.GetLength(1);
+            int largestValue = 0, dataSetJ = dataSet.GetLength(0), dataSetI = dataSet.GetLength(1);
 
             for(int j = 0; j < dataSet.GetLength(0); j++)
             {
@@ -22,22 +22,48 @@ namespace AdjacentProduct
 
                     if ((i + 1) < dataSetI && (j + 1) < dataSetJ)
                     {
-                        currentValue = dataSet[j, i] * dataSet[j, i + 1];
-                        largestValue = currentValue > largestValue ? currentValue : largestValue;
-                        currentValue = dataSet[j, i] * dataSet[j + 1, i];
-                        largestValue = currentValue > largestValue ? currentValue : largestValue;
-                        currentValue = dataSet[j, i] * dataSet[j + 1, i+1];
-                        largestValue = currentValue > largestValue ? currentValue : largestValue;
+                        largestValue = largestValueProduct((dataSet[j, i] * dataSet[j, i + 1]),
+                            (dataSet[j, i] * dataSet[j + 1, i]), (
+                            dataSet[j, i] * dataSet[j + 1, i + 1]));
                     }
                     if (j == dataSetJ - 1 && i+1 < dataSetI)
                     {
-                        currentValue = dataSet[j, i] * dataSet[j, i + 1];
-                        largestValue = currentValue > largestValue ? currentValue : largestValue;
-                        currentValue = dataSet[j, i] * dataSet[j - 1, i + 1];
-                        largestValue = currentValue > largestValue ? currentValue : largestValue;
+                        largestValue = largestValueProductBottom((dataSet[j, i] * dataSet[j, i + 1]),
+                            (dataSet[j, i] * dataSet[j - 1, i+1]));
                     }
                 }
-            }           
+            }
+            /// <summary>
+            /// This method goes throw the array to calculate the product
+            /// of adjacent values
+            /// </summary>
+            /// <param name="forward">input for one step ahead of the current index</param>
+            /// <param name="below">input for one step under the current index</param>
+            /// <param name="diagonal">input for one step diagonal from the current index</param>
+            /// <returns>the largestValue</returns>
+            int largestValueProduct(int forward, int below, int diagonal)
+            {
+
+                largestValue = forward > largestValue ? forward : largestValue;
+                largestValue = below > largestValue ? below : largestValue;
+                largestValue = diagonal > largestValue ? diagonal : largestValue;
+
+                return largestValue;
+            }
+            /// <summary>
+            /// This method is to account for the final row in the 2D array
+            /// </summary>
+            /// <param name="forward">input for one step ahead of the current index</param>
+            /// <param name="diagonal">input for one step diagonal from the current index</param>
+            /// <returns>largestValue</returns>
+            int largestValueProductBottom(int forward, int diagonal)
+            {
+
+                largestValue = forward > largestValue ? forward : largestValue;
+                largestValue = diagonal > largestValue ? diagonal : largestValue;
+                return largestValue;
+            }
+
             return largestValue;
         }
     }
