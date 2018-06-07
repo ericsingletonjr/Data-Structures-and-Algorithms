9281e03dcff06b94e2e3c0fdbe921309a00b823a
Eric Singleton <eric.singleton.jr@gmail.com>
2018-06-07 10:27:13 -0700

    Fixed a naming convention typo

--- Challenges/AdjacentProduct/AdjacentProductTests/UnitTest1.cs
+++ Challenges/AdjacentProduct/AdjacentProductTests/UnitTest1.cs
@@ -7,7 +7,7 @@ namespace AdjacentProductTests
     public class UnitTest1
     {
         [Fact]
-        public void largestValue1()
+        public void LargestValue1()
         {
             int[,] dataSet = new int[,] { { 1, 2, 1 }, { 2, 4, 2 }, { 3, 6, 8 }, { 7, 8, 1 } };
 
@@ -15,7 +15,7 @@ namespace AdjacentProductTests
         }
 
         [Fact]
-        public void largestValue2()
+        public void LargestValue2()
         {
             int[,] dataSet = new int[,] { { 6, 3, 5, 9 }, { 2, 8, 4, 6 }, { 3, 8, 2, 1 }, { 6, 7, 3, 5 } };
 
@@ -23,7 +23,7 @@ namespace AdjacentProductTests
         }
 
         [Fact]
-        public void largestValue3()
+        public void LargestValue3()
         {
             int[,] dataSet = new int[,] { {2,5,8,6,7 }, { 10,3,4,9,8}, 
                 { 1,6,15,11,2}, { 8,4,6,2,8} ,{ 9,20,2,4,10} };
