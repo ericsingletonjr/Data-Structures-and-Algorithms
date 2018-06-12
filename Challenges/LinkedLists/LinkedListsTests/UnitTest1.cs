e7c1f7e51f38157207c54fb0ef0e23c8f1d41e0f
Eric Singleton <eric.singleton.jr@gmail.com>
2018-06-12 09:19:16 -0700

    Added linkedList class and comments

---
+++ Challenges/LinkedLists/LinkedListsTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+using LinkedLists;
+using LinkedLists.Classes;
+
+namespace LinkedListsTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
