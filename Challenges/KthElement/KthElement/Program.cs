d24ca99743e7abd93b719d3cca74b99e1cba4907
Eric Singleton <eric.singleton.jr@gmail.com>
2018-06-12 13:54:12 -0700

    Added project base

---
+++ Challenges/KthElement/KthElement/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using KthElement.Classes;
+
+namespace KthElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Challenges/KthElement/KthElementTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+using KthElement;
+using KthElement.Classes;
+
+namespace KthElementTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
