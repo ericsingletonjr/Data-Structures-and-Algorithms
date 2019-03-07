9c072bd20572a27484093a6d29c419c31985e1b0
Eric Singleton <eric.singleton.jr@gmail.com>
2019-03-06 17:01:05 -0800

    Implemented PriorityQ with LL, needs tests

---
+++ Data_Structures/PriorityQueue/Classes/Node.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PriorityQueue.Classes
+{
+    public class Node
+    {
+        public int Priority { get; set; }
+        public int Value { get; set; }
+        public Node Next { get; set; }
+
+        public Node(int value, int priority)
+        {
+            Value = value;
+            Priority = priority;
+        }
+    }
+}
